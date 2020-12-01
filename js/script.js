43fc8f1682f67ebf895bebe6db5a2d6c15878659
Bogdan Lytvyn <boddofish@gmail.com>
2020-12-01 21:59:32 +0200

    Done

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        speed: 750,
+        easing: 'easy',
+        infinite: true,
+        initialSlides: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        waitForAnimate: false,
+        centerMode: true,
+        // rows: 2,
+        // slidesPerRows: 2,
+        responsive: [{
+            breakpoint: 768,
+            settings: { slidesToShow: 2,}
+               
+                    }
+        ],
+        mobileFirst:true,
+       
+    }
+       
+    );
+})
